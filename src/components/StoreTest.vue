affc07a772521f770fc3efd385660e237fd1fa1d
= <=>
2021-05-29 18:13:55 +0800

    22

--- src/components/StoreTest.vue
+++ src/components/StoreTest.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="StoreTest">
     count 数据 {{ appShowCount }}
-    <button @click="handleChange">改变数据 -- 测试55</button>
+    <button @click="handleChange">改变数据 -- 测试55666</button>
   </div>
 </template>
 
